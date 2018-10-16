a50e2bdfd0492e3923a55d3c3175de008f54ff70
hibby50 <hibby50@gmail.com>
2018-10-16 12:20:49 -0400

    Revert "Merge branch 'master' of https://github.com/hibby50/it490"
    This reverts commit 06ebce9776db4d002fb84798cea15605892494b1.

--- login.php
+++ login.php
@@ -3,53 +3,24 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+$db = mysqli_connect("127.0.0.1", "test", "1234", "test");
+
+if(!$db) {
+	echo "Error: unable to connect to mysql" . PHP_EQL;
+	echo "Debugging errno: " . mysqli_connect_error() . PHP_EOL;
+	echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+	exit;
+}
 
 $user = $_POST["user"];
 $password = $_POST["password"];
 
-/*if (mysqli_num_rows($result) > 0) {
+$result = mysqli_query($db,"SELECT username, password FROM users WHERE `username` = '".$user."' AND `password`= '".$password."'");
+
+if (mysqli_num_rows($result) > 0) {
     echo "Login Success";
     }
     else{
     echo "<img src='giphy.gif' alt='WRONG'>";
     }
-*/
-
-require_once('path.inc');
-require_once('get_host_info.inc');
-require_once('rabbitMQLib.inc');
-
-$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
-if (isset($argv[1]))
-{
-  $msg = $argv[1];
-}
-else
-{
-  $msg = "test message";
-}
-
-$request = array();
-$request['type'] = "login";
-$request['username'] = $user;
-$request['password'] = $password;
-//$request['message'] = $msg;
-$response = $client->send_request($request);
-//$response = $client->publish($request);
-
-echo "client received response: ".PHP_EOL;
-print_r($response);
-echo "\n\n";
-
-if ($response == 1){
-    echo "login success.";
-}
-else{
-    echo "login failure" . "<img src='/giphy.gif' alt='error'>";
-}
-
-echo $argv[0]." END".PHP_EOL;
-
 ?>
-
-
