6179ae0110fe8a137a9c0344f8d5dce29893932b
Andrew Hibberson <hibby50@gmail.com>
2018-10-26 16:56:37 -0400

    clean up log pruducer

--- logProducer.php
+++ logProducer.php
@@ -6,12 +6,6 @@ require_once('rabbitMQLib.inc');
 function doLog($error)
 {
 $client = new rabbitMQClient("rabbitMQLogging.ini","testServer");
-$msg = $error;
 
-
-$request = array();
-$request['message'] = $msg;
-//$response = $client->send_request($request);
-$client->publish($request);
-//file_put_contents("log.log", Date.$msg, FILE_APPEND); append a log file
+$client->publish($error);
 }
