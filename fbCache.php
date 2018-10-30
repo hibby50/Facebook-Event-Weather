34e13be4df17e3e01f2a23a199603194d2b83e1f
Andrew Hibberson <hibby50@gmail.com>
2018-10-30 17:00:00 -0400

    cacheing of facebook token

---
+++ fbCache.php
@@ -0,0 +1,17 @@
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+function doLog($error)
+{
+$client = new rabbitMQClient("dataCollectionDatabase.ini","testServer");
+$msg = $error;
+
+
+$request = array();
+$request['message'] = $msg;
+//$response = $client->send_request($request);
+$client->publish($request);
+//file_put_contents("log.log", Date.$msg, FILE_APPEND); append a log file
+}
